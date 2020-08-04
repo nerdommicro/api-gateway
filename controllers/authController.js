74349a62c10bea8834e670b4b0a509dc6360f5c2
michellenesbitt <mn218@nerdymail.com>
2020-08-04 11:17:30 -0400

    mn

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,13 +46,18 @@ exports.user_register = function (req, res) {
   };
   
   exports.user_token = function (req, res) {
-    console.log(req.userId)
-      User.getById(req.userId, function (err, user) { 
-        if (err)
-          return res.status(500).send('There was a problem finding the user.'); 
-        if (!user)
-          return res.status(404).send('No user found.'); 
-        res.status(200).send(user); 
-      });
+    let token = req.headers['x-access-token']
+  
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided'})
+  
+    jwt.verify(token, config.web.secret, function (err, decoded) {
+      if (err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.'})
+  
+      User.getById(decoded.id, function (err, user) {
+        if (err) return res.status(500).send('There was a problem finding the user.')
+        if (!user) return res.status(404).send('No user found.')
+        res.status(200).send(user)
+      })
+    })
   }
   // END OF NEW CODE
